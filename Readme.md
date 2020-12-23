0614afad3102c5adecedf509df62db2bec8301d1
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-12-23 12:32:17 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+https://discord.com/oauth2/authorize?client_id=791318841384108072&scope=bot
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
+
+use std::fs;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GENERAL_GROUP);
+
+    let token = fs::read_to_string("token.txt")
+        .expect("Failed to read token from file. Make sure that the file 'token.txt' is valid");
+
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
